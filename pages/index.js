359091efc131820eff95440de4fd586509b482be
Everton Lopes <contato@egsconsultores.com.br>
2025-12-06 00:11:20 +0000

    Docs: atualizar pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Home () {
     return (
-    <div style={{ border: '1px solid green', padding: '10px', margin: '10px', color: 'red' }}>
-    <h1>Sucesso é a única possibilidade!</h1></div>
+    <div style={{ border: '1px solid red', padding: '10px', margin: '10px', color: 'red' }}>
+    <h1>Olá, Mundo!
+        Sucesso é a única possibilidade!</h1></div>
     );
 }
 export default Home;
